cf2aedbd0aa06fce2f9af17a8e8d8daf27588c9e
nitinsingha273 <nitinsingha273@gmail.com>
2022-05-25 16:15:16 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import logo from './logo_transparent1.jpeg';
+import logo from './transprent-logo.png';
 import './App.css';
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
+        <br></br>
+          <br></br> <br></br><br></br>
+          
+         
         <img src={logo} className="App-logo" alt="logo" />
+        <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
         <div className='inputBox'>
-          <span>+91</span>
-          <input type="text" maxLength="10" id="mobile" placeholder="Enter Mobile Number"></input>
+          <button id="codebtn">+91</button>
+          <input   type="text" maxLength="10" id="mobile" placeholder="Enter Mobile Number"></input>
         </div>
         <div className="continueButton" onClick={()=>{
           let m=document.getElementById("mobile").value;
@@ -25,6 +34,17 @@ function App() {
             .then(response => console.log(response.json()))
             .then(data => console.log(data));
           }}>Continue</div>
+
+
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+
+          <div id ="comment">
+            <p>By continuing you agree to our</p>
+            <p id= "yellow"><b>Terms of Service & Privacy Policy</b></p>
+          </div>
       </header>
     </div>
   );
